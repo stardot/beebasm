ec52a02d24dedaec0bab22a2bfe30e03af2ec846
Steven Flintham <sgf@lemma.co.uk>
2021-04-08 19:32:05 +0100

    Add missing #include <algorithm>
    Thanks to tricky for pointing this out.
    diff --git a/src/commands.cpp b/src/commands.cpp
    index 3eb37cf..ce630bb 100644
    --- a/src/commands.cpp
    +++ b/src/commands.cpp
    @@ -22,6 +22,7 @@
     */
     /*************************************************************************************************/
    
    +#include <algorithm>
     #include <iostream>
     #include <iomanip>
     #include <string>

--- src/commands.cpp
+++ src/commands.cpp
@@ -22,6 +22,7 @@
 */
 /*************************************************************************************************/
 
+#include <algorithm>
 #include <iostream>
 #include <iomanip>
 #include <string>
