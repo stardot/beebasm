8317a33a4eac62c65e322192850570e001f9aa03
Charles Reilly <beebasm.2018@charlesreilly.com>
2021-07-13 10:09:03 +0100

    RNG - the values are 32-bit but the calculation is 64-bit
    Use uint_least32_t and uint_least64_t to make this explicit.  This
    breaks C++98 compatibility, but they come from C99 so nearly every
    C++98 compiler will support them.
    
    This is now producing the same random numbers on all platforms as
    previous 64-bit Linux builds.  Windows results have changed.

--- src/random.h
+++ src/random.h
@@ -24,12 +24,14 @@
 #ifndef RANDOM_H_
 #define RANDOM_H_
 
-#define BEEBASM_RAND_MULTIPLIER (48271U)
-#define BEEBASM_RAND_MODULUS (2147483647U)
-#define BEEBASM_RAND_MAX (BEEBASM_RAND_MODULUS - 2UL)
+#include <stdint.h>
 
-void beebasm_srand(unsigned int seed);
+#define BEEBASM_RAND_MULTIPLIER static_cast<uint_least32_t>(48271)
+#define BEEBASM_RAND_MODULUS static_cast<uint_least32_t>(2147483647)
+#define BEEBASM_RAND_MAX (BEEBASM_RAND_MODULUS - static_cast<uint_least32_t>(2))
 
-unsigned int beebasm_rand();
+void beebasm_srand(uint_least32_t seed);
+
+uint_least32_t beebasm_rand();
 
 #endif // RANDOM_H_
